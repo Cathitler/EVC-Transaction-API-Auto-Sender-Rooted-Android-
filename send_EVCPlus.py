4a9d747b11b4532eeb79b1a28d9eafafdb201d49
Joseph <160449473+Cathitler@users.noreply.github.com>
2025-08-14 22:18:46 +0300

    Update send_EVCPlus.py

--- send_EVCPlus.py
+++ send_EVCPlus.py
@@ -4,4 +4,5 @@ def send_evc(number, amount):
     subprocess.run(["/data/data/com.termux/files/home/send_evc.sh", number, str(amount)])
 
 # Example
-send_evc("615925885", 5)
+send_evc("615555555", 5)
+#replace the number with the number you want to send to or auto it in your app/server, and the 5 is the amount
